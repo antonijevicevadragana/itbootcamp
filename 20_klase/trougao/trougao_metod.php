bce9b84f0b34ba0856f3762aa62688609ba2c8f3
antonijevicevadragaba <antonijevicd.1991@gmail.com>
2023-05-30 17:48:41 +0200

    trougao_static metord

---
+++ 20_klase/trougao/trougao_metod.php
@@ -0,0 +1,101 @@
+<?php
+
+class Trougao
+{
+    private $a;
+    private $b;
+    private $c;
+
+    private static function uslovZaTrougao($a, $b, $c) {
+        return ($a > 0 && $b > 0 && $c > 0 && $a + $b > $c && $a + $c > $b && $b + $c > $a);
+    }
+
+    //construktor
+
+    public function __construct($a, $b, $c)
+    {
+
+         if (self::uslovZaTrougao($a, $b, $c)) {
+            $this->a=$a;
+            $this->b=$b;
+            $this->c=$c;
+        }
+        else{
+            $this->a=0;
+            $this->b=0;
+            $this->c=0;
+        }
+    }
+
+
+    //seteri
+
+    public function setA($a)
+    {
+        if (self::uslovZaTrougao($a, $this->b, $this->c)) {
+            $this->a = $a;
+        }
+        else {
+            echo "Ne moze da se promeni vrednost stranie a";
+        }
+    }
+
+    public function setB($b)
+    {
+        if (self::uslovZaTrougao($this->a, $b, $this->c)) {
+            if ($b > 0 ) {
+            $this->b = $b;
+        }
+        else {
+            echo "Ne moze da se promeni vrednost stranie b";
+        }
+    }
+}
+
+    public function setC($c)
+    {
+        if (self::uslovZaTrougao($this->a, $this->b, $c)) {
+            $this->c = $c;
+        }
+        // else {
+        //     $this->c = 0;
+        //     $this->a=0;
+        //     $this->b=0;
+        // }
+        else {
+            echo "Ne moze da se promeni vrednost stranie c";
+        }
+    }
+
+    //seteri
+
+    public function getA()
+    {
+        return  $this->a;
+    }
+
+    public function getB()
+    {
+        return  $this->b;
+    }
+
+    public function getC()
+    {
+        return  $this->c;
+    }
+
+
+    //metode
+
+    public function obimTrougla()
+    {
+        $obim = ($this->a + $this->b + $this->c);
+        return $obim;
+    }
+
+    public function povrsinaTrougla()
+    {
+        $povrsina = (sqrt(($this->a + $this->b + $this->c) * ($this->a + $this->b - $this->c) * ($this->b + $this->c - $this->a) * ($this->c + $this->a - $this->b))) / 4;
+        return $povrsina;
+    }
+}
